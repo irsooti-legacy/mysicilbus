b191afa53d3fc0719dd1efc6fd5927d36aa368c4
Daniele <irsooti@gmail.com>
2019-08-24 00:15:36 +0200

    Update PullmanRides.js

--- src/PullmanRides/PullmanRides.js
+++ src/PullmanRides/PullmanRides.js
@@ -4,11 +4,11 @@ import PullmanRide from '../PullmanRide/PullmanRide';
 const PullmanRides = (props) => {
     return (
         <div style={{ marginTop: '20px' }}>
-            {props.rides ? props.rides.error ? <div className="hero is-size-1 is-medium">
+            {props.rides ? props.rides.error ? <div className="hero is-medium">
                 <div className="hero-body has-text-centered">
                     <div style={{ fontSize: '4em' }}><i class="fas fa-grin-beam-sweat"></i></div>
-                    <div>Non ho trovato nessuna corsa!</div>
-                    <div className="is-size-6"><small>Sicuro di aver compilato tutti i campi?</small></div>
+                    <div style={{ fontWeight: "bolder", fontSize: "xx-large" }}>Non ho trovato nessuna corsa!</div>
+                    <div className="is-size-6"><small>Capita, non prendertela con me.</small></div>
                 </div>
             </div> : props.rides.results.map((r, i) => <PullmanRide key={i} {...r} />) : null}
         </div >
